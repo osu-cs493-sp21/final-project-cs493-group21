4077d9763efd50b15980632cca7885ce04b1cc50
Kevin Neiger <neigerk@oregonstate.edu>
2021-06-09 14:35:32 -0700

    Albums POST, GET and Artists/id/Albums GET endpoints created

--- api/albums.js
+++ api/albums.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
-
+const { generateAuthToken,
+        requireAuthentication,
+        requireAuthentication_createUser } = require('../lib/auth');
+const { AlbumSchema,
+        getAlbumById,
+        insertNewAlbum } = require('../models/album');
+const { validateAgainstSchema } = require('../lib/validation');
 //admin auth
 //router.post()
+//router.post('/', requireAuthentication, async (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  if(validateAgainstSchema(req.body, AlbumSchema)){
+    const album = {
+      name: req.body.name,
+      artistid: req.body.artistid,
+      songs: req.body.songs
+    }
+
+    try {
+      const id = await insertNewAlbum(album);
+      res.status(201).send({
+        id: id,
+        links: {
+          album: `/albums/${id}`,
+          artistsAlbums: `/artists/${req.body.artistid}/albums`
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        error:  "Error inserting album into DB.  Please try again later."
+      })
+    }
+  } else {
+    res.status(400).send({
+      error: "Request body is not a valid song object"
+    });
+  }
+})
 
 //admin auth
 //router.patch()
@@ -14,6 +50,7 @@ router.get('/:id', async (req, res, next) => {
       const responseBody = {
         _id: album._id,
         albumName: album.name,
+        artistid: album.artistid,
         songs: album.songs
       }
       res.status(200).send(responseBody);
--- api/artists.js
+++ api/artists.js
@@ -1,4 +1,5 @@
 const { getArtistsPage, getArtistById } = require('../models/artist');
+const { getAlbumsByArtistId } = require('../models/album');
 
 const router = require('express').Router();
 
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
      * Fetch page info about all artists
      */
     const artistPage = await getArtistsPage(parseInt(req.query.page) || 1);
-    
+
     /*
      * Generate HATEOAS links for surrounding pages
      */
@@ -22,12 +23,12 @@ router.get('/', async (req, res) => {
       artistPage.links.prevPage = `artists?page=${artistPage.page - 1}`;
       artistPage.links.firstPage = `artists?page=1`;
     }
-    
+
     /*
      * Return the page of all artists as the response
      */
     res.status(200).send(artistPage);
-    
+
   }catch (err) {
     console.error(err);
     res.status(500).send({
@@ -49,6 +50,30 @@ router.get('/:id', async (req, res, next) => {
     res.status(500).send({
       error: "Error fetching artist using id. Please try again later."
     });
+  }
+});
+
+/*
+ * Endpoint for getting a list of album ids that belong to an artist
+ */
+router.get('/:id/albums', async (req, res, next) => {
+  console.log("requested album by id:", req.params.id);
+  try {
+    const albums = await getAlbumsByArtistId(req.params.id);
+    if (albums) {
+      const responseBody = {
+        artistid: req.params.id,
+        albumids: albums
+      }
+      res.status(200).send(responseBody);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: "Unable to fetch albums for that artist.  Please try again later."
+    })
   }
 });
 
